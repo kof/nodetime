3f80861ff943aff79d735e8941608edb059ace30
Dmitri Melikyan <dmitri@rapidlogix.com>
2012-03-27 16:55:05 +0200

    important probe fixes

--- test/probes/http.js
+++ test/probes/http.js
@@ -27,16 +27,48 @@ var http = require('http');
 var https = require('https');
 
 module.exports = function(cb) {
-  http.get({hostname: 'search.cliqz.com'}, function(cRes) {
-    cRes.on('end', function() { 
-      cb();
+
+  function error() {
+    var client = http.get({hostname: 'nonexisting'}, function(cRes) {
+      cRes.on('end', function() { 
+        success();
+      });
+
+      cRes.on('error', function(err) {
+        console.error(err);
+
+        success();
+      });
     });
 
-    cRes.on('error', function(err) {
+    client.on('error', function(err) {
+      console.error(err);
+
+      success();
+    });
+  };
+
+
+  function success() {
+    var client = http.get({hostname: 'search.cliqz.com'}, function(cRes) {
+      cRes.on('end', function() { 
+        cb();
+      });
+
+      cRes.on('error', function(err) {
+        console.error(err);
+
+        cb();
+      });
+    });
+
+    client.on('error', function(err) {
       console.error(err);
 
       cb();
     });
-  });
+  };
+
+  error();
 };
 
