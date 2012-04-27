85961ca2be37160775d44bb4c911f7e67c1418d8
Dmitri Melikyan <dmitri@rapidlogix.com>
2012-04-27 10:12:04 +0200

    deleted test files

--- prof2.js
+++
@@ -1,9 +0,0 @@
-var d = require('dtrace-provider');
-
-var dtp = d.createDTraceProvider("nodeapp");
-dtp.addProbe("probe1", "int", "int");
-dtp.addProbe("probe2", "char *");
-dtp.enable();      
-dtp.fire("probe1", function() { return [1, 2]; });
-dtp.fire("probe2", function() { return ["hello, dtrace"]; });
-
